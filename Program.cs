a7927979b702e61cf9d373593ed89da903f6d11c
kwamekyeimonies <kwamekyeimonies@gmail.com>
2023-05-14 03:08:57 +0000

    debugging mode

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using WatchDog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddWatchDogServices(
+    opt =>
+    {
+        opt.IsAutoClear = false;
+        opt.SetExternalDbConnString = builder.Configuration.GetConnectionString("PostgreSQL_Connection");
+        opt.SqlDriverOption = WatchDogSqlDriverEnum.PostgreSql;
+    }
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseWatchDogExceptionLogger();
+app.UseWatchDog(
+    settings =>
+    {
+        settings.WatchPagePassword = "admin123";
+        settings.WatchPageUsername = "monies@001";
+    }
+);
+
+app.Run();
---
+++ Readme.md
@@ -0,0 +1,3 @@
+WatchDog .Net log Monitoring with Postgresql
+
+Bug:'WatchDogSettings' does not contain a definition for 'SqlDriverOption' and no accessible extension method 'SqlDriverOption' accepting a first argument of type 'WatchDogSettings' could be found (are you missing a using directive or an assembly reference?)
